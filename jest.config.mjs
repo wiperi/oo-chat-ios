a8d0fd2e2653a4ebb73b53d4c822859cf418d174
wiperi <solidtreepassing@gmail.com>
2026-07-06 16:33:25 +1000

    refactor: delete RN version code
    Signed-off-by: wiperi <solidtreepassing@gmail.com>

--- jest.config.mjs
+++
@@ -1,8 +0,0 @@
-export default {
-  preset: 'react-native',
-  setupFiles: ['<rootDir>/jest.setup.js'],
-  transformIgnorePatterns: [
-    'node_modules/(?!\\.pnpm|((jest-)?react-native|@react-native(-community)?)/)',
-    'node_modules/.pnpm/(?!(react-native|@react-native\\+|@react-native-community\\+))',
-  ],
-};
--- jest.setup.js
+++
@@ -1,2 +0,0 @@
-/* eslint-env jest */
-jest.mock('react-native-get-random-values', () => ({}));
